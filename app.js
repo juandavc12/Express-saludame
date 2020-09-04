5a4f1c991697da9e37ae566f9a55f1b372ccb155
Juan <juandavc12@hotmail.com>
2020-09-03 21:01:23 -0500

    .

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express ();
 
 app.get('/makers/:nombre', (req, res) => {
     let nombre = req.params.nombre;
-    // if( nombre === '' || nombre === undefined){
-    //     res.send('<h1>Hola desconocido!</h1>')
-    // }
-    // else{
+    if( nombre === '' || nombre === undefined){
+        res.send('<h1>Hola desconocido!</h1>')
+    }
+    else{
     res.send('<h1>Hola ' + nombre +  '!</h1>')
-    // }
+    }
 });
 
 app.listen(3000, () => console.log('listening in port 3000 xD'));
