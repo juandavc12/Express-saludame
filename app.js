d8d8ec24d566dbd067b77d9c83f9e5904205be73
Juan <juandavc12@hotmail.com>
2020-09-04 15:14:46 -0500

    .params _capitalize

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express ();
 
 app.get('/makers/:nombre', (req, res) => {
     let nombre = req.params.nombre;
-    if( nombre === '' || nombre === undefined){
-        res.send('<h1>Hola desconocido!</h1>')
-    }
-    else{
-    res.send('<h1>Hola ' + nombre +  '!</h1>')
-    }
+    // if( nombre === '' || nombre === undefined){
+    //     res.send('<h1>Hola desconocido!</h1>')
+    // }
+    // else{
+        function capitalizeFirstLetter(string) {
+            const name = string.charAt(0).toUpperCase() + string.slice(1);
+            res.send('<h1>Hola ' + name +  '!</h1>')
+        }
+    capitalizeFirstLetter(nombre)
+    // }
 });
 
 app.listen(3000, () => console.log('listening in port 3000 xD'));
